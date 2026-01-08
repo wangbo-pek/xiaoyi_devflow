beb8cada87ea8a90aca3b71e3cf85055bbc2a5af
wangbo-pek <wangbo.pek@gmail.com>
2026-01-08 16:56:45 +0800

    implement action handler

---
+++ lib/handlers/action.ts
@@ -0,0 +1,57 @@
+"use server";
+
+import { ZodError, ZodSchema } from "zod/v3";
+import { UnauthorizedError, ValidationError } from "../http-errors";
+import { Session } from "next-auth";
+import { auth } from "@/auth";
+import dbConnect from "../mongoose";
+
+type ActionOptions<T> = {
+    params?: T;
+    schema: ZodSchema<T>;
+    authorize?: boolean;
+};
+
+// 1. 检查参数是否符合schema
+// 2. 检查是否授权
+// 3. 连接数据库
+// 4. 返回参数和session
+
+async function action<T>({
+    params,
+    schema,
+    authorize = false,
+}: ActionOptions<T>) {
+    if (schema && params) {
+        try {
+            schema.parse(params);
+        } catch (error) {
+            if (error instanceof ZodError) {
+                const formattedErrors: Record<string, string[]> = {};
+                error.issues.forEach((issue) => {
+                    const path = issue.path.join(".");
+                    if (!formattedErrors[path]) {
+                        formattedErrors[path] = [];
+                    }
+                    formattedErrors[path].push(issue.message);
+                });
+                throw new ValidationError(formattedErrors);
+            } else {
+                return new Error("Schema validation failed");
+            }
+        }
+    }
+
+    let session: Session | null = null;
+    if (authorize) {
+        session = await auth();
+        if (!session) {
+            throw new UnauthorizedError();
+        }
+    }
+    await dbConnect();
+
+    return { params, session };
+}
+
+export default action;
